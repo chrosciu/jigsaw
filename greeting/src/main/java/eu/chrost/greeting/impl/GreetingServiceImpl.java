10463f1c521e9c22e6bcc5e6785f112fe1cce88f
Marcin Chrost <mchrost82@gmail.com>
2024-02-24 16:13:35 +0100

    Added commons lang 3 as automatic module

--- greeting/src/main/java/eu/chrost/greeting/impl/GreetingServiceImpl.java
+++ greeting/src/main/java/eu/chrost/greeting/impl/GreetingServiceImpl.java
@@ -2,12 +2,16 @@ package eu.chrost.greeting.impl;
 
 import eu.chrost.greeting.GreetingService;
 import eu.chrost.upper.UpperCaseService;
+import org.apache.commons.lang3.ObjectUtils;
 
 public class GreetingServiceImpl implements GreetingService {
 
     @Override
     public String greet(String name) {
-        return "Hello " + name + "!";
+        if (!ObjectUtils.isEmpty(name)) {
+            return "Hello " + name + "!";
+        }
+        throw new IllegalStateException("Empty name passed as argument!");
     }
 
     @Override
--- greeting/src/main/java/module-info.java
+++ greeting/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module eu.chrost.greeting {
     exports eu.chrost.greeting;
     requires eu.chrost.upper;
+    requires org.apache.commons.lang3;
 }
